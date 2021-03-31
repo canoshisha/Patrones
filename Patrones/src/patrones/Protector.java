97ed979bf388d389008dd168ed5d91e9d0028302
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2021-03-31 13:28:27 +0200

    Update Protector.java

--- Patrones/src/patrones/Protector.java
+++ Patrones/src/patrones/Protector.java
@@ -2,6 +2,30 @@
 package patrones;
 
 
-public class Protector {
+public class Protector extends Champion {
+    
+    public String special_move;
+            
+    public Protector(String alias,String skin,String special_move){
+        setAlias(alias);
+        setAttack(80);
+        setDefense(180);
+        setHealth(200);
+        setSkin(skin);
+        setSpecial_move(special_move);
+    }
+
+    public String getSpecial_move() {
+        return ""+getAlias()+" ha usado su habilidad "+ special_move;
+    }
+
+    public void setSpecial_move(String special_move) {
+        if(special_move.equalsIgnoreCase("shield") || special_move.equalsIgnoreCase("blockage") ||special_move.equalsIgnoreCase("freezer bomb") ){
+        this.special_move=special_move;
+    }else{
+            System.out.println("No se ha seleccionado una habilidad compatible con este campeón, por defecto será shield");
+            this.special_move="shield";
+        }
+    }
     
 }
