1a3ba68fc6d81e848ae569da9688bb513b0c9439
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2021-03-31 13:28:23 +0200

    Update Controller.java

--- Patrones/src/patrones/Controller.java
+++ Patrones/src/patrones/Controller.java
@@ -2,6 +2,30 @@
 package patrones;
 
 
-public class Controller {
+public class Controller extends Champion{
+    
+      public String special_move;
+            
+    public Controller(String alias,String skin,String special_move){
+        setAlias(alias);
+        setAttack(120);
+        setDefense(130);
+        setHealth(150);
+        setSkin(skin);
+        setSpecial_move(special_move);
+    }
+
+    public String getSpecial_move() {
+        return ""+getAlias()+" ha usado su habilidad "+ special_move;
+    }
+
+    public void setSpecial_move(String special_move) {
+        if(special_move.equalsIgnoreCase("smoke bomb") || special_move.equalsIgnoreCase("stealth") ||special_move.equalsIgnoreCase("invisibility") ){
+        this.special_move=special_move;
+    }else{
+            System.out.println("No se ha seleccionado una habilidad compatible con este campeón, por defecto será smoke bomb");
+            this.special_move="smoke bomb";
+        }
+    }
     
 }
