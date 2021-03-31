aab02cae7c22ce16dad9f05fe2ebca7fb1e70138
matero1999 <matero1999@gmail.com>
2021-03-31 12:40:32 +0200

    Actualizacion

--- Patrones/src/patrones/IGame.java
+++ Patrones/src/patrones/IGame.java
@@ -1,7 +1,23 @@
-
 package patrones;
 
-
 public interface IGame {
-    
+
+    public String getNombre();
+
+    public String getEmpresa();
+
+    public int getNumChampions();
+
+    public boolean isMultiplayer();
+
+    public void setNombre(String nombre);
+
+    public void setEmpresa(String empresa);
+
+    public void setNumChampions(int numChampions);
+
+    public void setMultiplayer(boolean multiplayer);
+
+    @Override
+    public String toString();
 }
--- Patrones/src/patrones/Platform.java
+++ Patrones/src/patrones/Platform.java
@@ -2,6 +2,22 @@
 package patrones;
 
 
-public class Platform {
+public class Platform implements Observable{
+
+    
+    @Override
+    public void registerObserver(Observer o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void notifyObservers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
