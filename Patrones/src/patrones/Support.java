2ce7de9ba328c8dbf79291237406947311b6a46d
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2021-03-31 13:28:31 +0200

    Update Support.java

--- Patrones/src/patrones/Support.java
+++ Patrones/src/patrones/Support.java
@@ -2,6 +2,32 @@
 package patrones;
 
 
-public class Support {
+public class Support extends Champion{
+
+public String special_move;
+            
+    public Support(String alias,String skin,String special_move){
+        setAlias(alias);
+        setAttack(95);
+        setDefense(100);
+        setHealth(150);
+        setSkin(skin);
+        setSpecial_move(special_move);
+    }
+
+    public String getSpecial_move() {
+        return ""+getAlias()+" ha usado su habilidad "+ special_move;
+    }
+
+    public void setSpecial_move(String special_move) {
+        if(special_move.equalsIgnoreCase("medical kit") || special_move.equalsIgnoreCase("bandages") ||special_move.equalsIgnoreCase("curative potion") ){
+        this.special_move=special_move;
+    }else{
+            System.out.println("No se ha seleccionado una habilidad compatible con este campeón, por defecto será medical kit");
+            this.special_move="medical kit";
+        }
+    }
+
+
     
 }
