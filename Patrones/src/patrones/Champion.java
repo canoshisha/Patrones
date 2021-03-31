55afec4ed548875b8f37898e4a5c134e172cfe44
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2021-03-31 12:19:27 +0200

    Update Champion.java

--- Patrones/src/patrones/Champion.java
+++ Patrones/src/patrones/Champion.java
@@ -4,4 +4,52 @@ package patrones;
 
 public class Champion {
     
+    public int health,attack,defense;
+    public String skin,alias;
+    
+    public Champion(){
+        
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public String getSkin() {
+        return skin;
+    }
+
+    public void setSkin(String skin) {
+        this.skin = skin;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+    
+    
 }
