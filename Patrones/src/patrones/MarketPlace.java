aa7b0bb961724d0146a48d64a5013cb486233ed8
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2021-04-02 11:05:50 +0200

    Update MarketPlace.java

--- Patrones/src/patrones/MarketPlace.java
+++ Patrones/src/patrones/MarketPlace.java
@@ -1,7 +1,46 @@
 
 package patrones;
-
+import java.util.*;
 
 public class MarketPlace {
     
+    List<Game> games_list;
+
+    public MarketPlace() {
+        
+    }
+    
+    public void addGame(Game game){
+        games_list.add(game);
+    }
+    
+    public List<Game> getLista_juegos(){
+        return games_list;
+    }
+    public boolean removeGame(String name){
+        Iterator game = games_list.iterator();
+        boolean terminado = false;
+        Game actual = (Game) game;
+        Game searched = null;
+        if(actual.getNombre().compareToIgnoreCase(name)==0){
+            searched = actual;
+            terminado = true;
+        }else {
+            while (game.hasNext()) {
+                game = (Iterator) game.next();
+                actual = (Game) game;
+                if (actual.getNombre().compareToIgnoreCase(name) == 0) {
+                    searched = actual;
+                    terminado = true;
+                }
+
+            }
+
+        }if (terminado == true) {
+            return games_list.remove(searched);
+        } else {
+            return false;
+        }
+        
+    }
 }
