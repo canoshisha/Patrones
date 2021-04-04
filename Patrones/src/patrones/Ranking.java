f53c72e55828c3e3747da437aefade3ee5300b63
matero1999 <matero1999@gmail.com>
2021-04-04 10:39:09 +0200

    Actualizaciones

--- Patrones/src/patrones/Ranking.java
+++ Patrones/src/patrones/Ranking.java
@@ -4,18 +4,23 @@ import java.util.*;
 
 public class Ranking {
     
-    List<Player> players_list;
+    Set<Player> players_list;
 
     public Ranking() {
-        
+        players_list = new HashSet<>();
     }
     
     public boolean addPlayer(Player new_player){
-        boolean exito = false;
-        if(!players_list.isEmpty()){
+        return players_list.add(new_player);
         
     }
-        
+    public boolean removePlayer(Player player){
+        return players_list.remove(player);
     }
+
+    public Set<Player> getPlayers_list() {
+        return players_list;
+    }
+    
     
 }
