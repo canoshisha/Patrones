c2c862c95c08c18dccf5003eb20a2b0f303cb340
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2021-04-04 11:44:50 +0200

    Update Champion_Customizer.java

--- Patrones/src/patrones/Champion_Customizer.java
+++ Patrones/src/patrones/Champion_Customizer.java
@@ -3,5 +3,29 @@ package patrones;
 
 
 public class Champion_Customizer {
+
+    public Champion_Customizer() {
+    }
     
+    public Champion createChampion(int code,String alias,String skin, String special_move){
+        Champion basic_champion=null;
+        //Creación de support
+        switch (code) {
+            case 1:
+                basic_champion = new Support(alias,skin, special_move);
+                break;
+            case 2:
+                basic_champion = new Protector(alias,skin, special_move);
+                break;
+            case 3:
+                basic_champion = new Entry(alias,skin, special_move);
+                break;
+            case 4:
+                basic_champion = new Controller(alias, skin, special_move);
+                break;
+            default:
+                break;
+        }
+        return basic_champion;
+    }
 }
